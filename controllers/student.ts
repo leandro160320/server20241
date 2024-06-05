856d199a0ee349ca395a3957076868a08212b3f3
leandro160320 <148804278+leandro160320@users.noreply.github.com>
2024-06-05 00:49:52 +0000

    feat: Students

---
+++ controllers/student.ts
@@ -0,0 +1,22 @@
+import { pool } from "../shared/database";
+import { Request, Response, Router } from "express";
+
+export async function listStudent(req: Request, res: Response) {
+  //conecta com o banco
+  const client = await pool.connect();
+  //realiza consulta sql
+  const students = await client.query(`select * from students`)
+  //retorna consulta em formato json
+  return res.status(200).json(students.rows);
+}
+export async function saveStudent(req: Request, res: Response) {
+  const student  = req.body;
+  console.log(student)
+  const client = await pool.connect();
+  //realiza consulta sql
+  const response = await client.query(`INSERT INTO courses (name) VALUES ('${student.name}')`)
+  res.status(201).json(response);
+}
+
+
+
--- router.ts
+++ router.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { listCourse, saveCourse } from "./controllers/course";
+import { listStudent, saveStudent } from "./controllers/student";
 const router = Router();
 
 router.get("/courses", listCourse);
 router.post("/courses", saveCourse)
+
+router.get("/students", listStudent);
+router.post("/students", saveStudent)
 
 export { router };
