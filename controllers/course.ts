686dbf484242abb6e50b5dcd51f8126344a244b2
leandro160320 <148804278+leandro160320@users.noreply.github.com>
2024-06-11 23:23:17 +0000

    fix: try

--- controllers/course.ts
+++ controllers/course.ts
@@ -9,20 +9,28 @@ export async function listCourse(req: Request, res: Response) {
     const courses = await client.query(`select * from courses`)
     //retorna consulta em formato json
     return res.status(200).json(courses.rows);
-  }catch(error){
-    console.log (error)
-  }finally{
+  } catch (error) {
+    console.log(error)
+  } finally {
     client.release
   }
-  
+
 }
 export async function saveCourse(req: Request, res: Response) {
   const course = req.body;
   console.log(course)
   const client = await pool.connect();
   //realiza consulta sql
-  const response = await client.query(`INSERT INTO courses (name) VALUES ('${course.name}')`)
-  res.status(201).json(response);
+  try {
+    const response = await client.query(`INSERT INTO courses (name) VALUES ('${course.name}')`)
+    res.status(200).json(course.rows)
+  } catch (error) {
+    res.status(400).json({ message: 'Dados invalidos', error });
+  } finally {
+    client.release()
+  }
+
+  
 }
 
 
--- controllers/student.ts
+++ controllers/student.ts
@@ -5,25 +5,31 @@ export async function listStudent(req: Request, res: Response) {
   //conecta com o banco
   const client = await pool.connect();
   try {
-//realiza consulta sql
-  const students = await client.query(`select * from students`)
-//retorna consulta em formato json
-  return res.status(200).json(students.rows);
-  }catch(error){
-    console.log (error)
-  }finally{
+    //realiza consulta sql
+    const students = await client.query(`select * from students`)
+    //retorna consulta em formato json
+    return res.status(200).json(students.rows);
+  } catch (error) {
+    console.log(error)
+  } finally {
     client.release
   }
-  
-  
-export async function saveStudent(req: Request, res: Response) {
-  const student  = req.body;
-  console.log(student)
-  const client = await pool.connect();
-  //realiza consulta sql
-  const response = await client.query(`INSERT INTO students (name, email) VALUES ('${student.name}', '${student.email}')`)
-  res.status(201).json(response);
 }
 
+export async function saveStudent(req: Request, res: Response) {
+  const student = req.body;
+  console.log(student)
 
+  const client = await pool.connect();
+  //realiza consulta sql
+  try {
+    const response = await client.query(`INSERT INTO students (name, email) VALUES ('${student.name}', '${student.email}')`)
+    console.log(response.rows[0]);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(400).json({ message: 'Dados invalidos', error });
+  } finally {
+    client.release()
+  }
 
+}
