ac00546c1ccfa6204364bcf05e029edfbff10ef7
leandro160320 <148804278+leandro160320@users.noreply.github.com>
2024-06-12 00:10:08 +0000

    fix: Delete

--- router.ts
+++ router.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import { listCourse, saveCourse, deleteCourses } from "./controllers/course";
-import { listStudent, saveStudent, deleteStudents } from "./controllers/student";
+import { listCourse, saveCourse, deleteCourse } from "./controllers/course";
+import { listStudent, saveStudent, deleteStudent } from "./controllers/student";
 const router = Router();
 
 router.get("/courses", listCourse);
 router.post("/courses", saveCourse);
-router.delete("/courses", deleteCourses);
+router.delete("/courses/:id", deleteCourse);
 
 router.get("/students", listStudent);
 router.post("/students", saveStudent);
-router.delete("/students/:id", deleteStudents)
+router.delete("/students/:id", deleteStudent)
 
 export { router };
