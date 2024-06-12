f09cc80116f375c51dd37021f06c08827869b808
leandro160320 <148804278+leandro160320@users.noreply.github.com>
2024-06-12 19:53:47 +0000

    fix: Professor

--- controllers/teacher.ts
+++ controllers/teacher.ts
@@ -6,7 +6,7 @@ export async function deleteTeacher(req: Request, res: Response) {
   const client = await pool.connect();
   const id = req.params.id
   try {
-    const response = await client.query(`delete from teachers where id=${id}`);
+    const response = await client.query(`delete from students where id=${id}`);
     res.status(200).json({ message: "Registro Excluido"})
   } catch (error) {
     res.status(404).json({message:error})
@@ -17,8 +17,8 @@ export async function deleteTeacher(req: Request, res: Response) {
 export async function listTeacher(req: Request, res: Response) {
   //conecta com o banco
   const client = await pool.connect();
-  //realiza consulta sql
   try {
+    //realiza consulta sql
     const teachers = await client.query(`select * from teachers`)
     //retorna consulta em formato json
     return res.status(200).json(teachers.rows);
@@ -27,24 +27,22 @@ export async function listTeacher(req: Request, res: Response) {
   } finally {
     client.release
   }
-
 }
+
 export async function saveTeacher(req: Request, res: Response) {
   const teacher = req.body;
   console.log(teacher)
+
   const client = await pool.connect();
   //realiza consulta sql
   try {
-    const response = await client.query(`INSERT INTO teachers (name) VALUES ('${teacher.name}')`)
-    res.status(200).json(teacher.rows)
+    const response = await client.query(`INSERT INTO teachers (name, email) VALUES ('${teacher.name}', '${teacher.email}')`)
+    console.log(response.rows[0]);
+    res.status(201).json(response);
   } catch (error) {
     res.status(400).json({ message: 'Dados invalidos', error });
   } finally {
     client.release()
   }
 
-  
 }
-
-
-
