0624fbd4ba28845771671e898c39bd5b78a688c0
leandro160320 <148804278+leandro160320@users.noreply.github.com>
2024-06-13 16:58:29 +0000

    fix: Subject

--- router.ts
+++ router.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { listCourse, saveCourse, deleteCourse } from "./controllers/course";
 import { listStudent, saveStudent, deleteStudent } from "./controllers/student";
 import { listTeacher, saveTeacher, deleteTeacher } from "./controllers/teacher";
-import { listSubject, saveSubject, deleteSubject } from "./controllers/teacher";
+import { listSubject, saveSubject, deleteSubject } from "./controllers/subject";
 const router = Router();
 
 router.get("/courses", listCourse);
