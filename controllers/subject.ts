ba878a78b0021ce884644264411034afc9c584f3
leandro160320 <148804278+leandro160320@users.noreply.github.com>
2024-06-13 16:47:55 +0000

    feat: Subjects

---
+++ controllers/subject.ts
@@ -0,0 +1,50 @@
+import { pool } from "../shared/database";
+import { Request, Response, Router } from "express";
+
+export async function deleteSubject(req: Request, res: Response) {
+  //conecta com o banco
+  const client = await pool.connect();
+  const id = req.params.id
+  try {
+    const response = await client.query(`delete from subjects where id=${id}`);
+    res.status(200).json({ message: "Registro Excluido"})
+  } catch (error) {
+    res.status(404).json({message:error})
+  }finally {
+    client.release
+  }
+}
+export async function listSubject(req: Request, res: Response) {
+  //conecta com o banco
+  const client = await pool.connect();
+  //realiza consulta sql
+  try {
+    const subjects = await client.query(`select * from subjects`)
+    //retorna consulta em formato json
+    return res.status(200).json(subjects.rows);
+  } catch (error) {
+    console.log(error)
+  } finally {
+    client.release
+  }
+
+}
+export async function saveSubjects(req: Request, res: Response) {
+  const subject = req.body;
+  console.log(subject)
+  const client = await pool.connect();
+  //realiza consulta sql
+  try {
+    const response = await client.query(`INSERT INTO subjects (name) VALUES ('${subject.name}')`)
+    res.status(200).json(subject.rows)
+  } catch (error) {
+    res.status(400).json({ message: 'Dados invalidos', error });
+  } finally {
+    client.release()
+  }
+
+  
+}
+
+
+
--- router.ts
+++ router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { listCourse, saveCourse, deleteCourse } from "./controllers/course";
-import { listStudent, saveStudent, deleteStudent } from "./controllers/student";
-import { listTeacher, saveTeacher, deleteTeacher } from "./controllers/teacher";
+import { listCourse, saveCourse, deleteCourse, listSubject } from "./controllers/course";
+import { listStudent, saveStudent, deleteStudent, saveSubject } from "./controllers/student";
+import { listTeacher, saveTeacher, deleteTeacher,deleteSubjec } from "./controllers/teacher";
 const router = Router();
 
 router.get("/courses", listCourse);
@@ -15,5 +15,9 @@ router.delete("/students/:id", deleteStudent);
 router.get("/teachers", listTeacher);
 router.post("/teachers", saveTeacher);
 router.delete("/teachers/:id", deleteTeacher)
+
+router.get("/teachers", listSubject);
+router.post("/teachers", saveSubject);
+router.delete("/teachers/:id", deleteSubjec)
 
 export { router };
