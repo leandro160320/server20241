bfe4d508b697e81ab4d09d75b59958633c6a9baf
leandro160320 <148804278+leandro160320@users.noreply.github.com>
2024-06-18 23:55:44 +0000

    fix: Delete components

--- router.ts
+++ router.ts
@@ -1,32 +1,9 @@
 import { Router } from "express";
-import { listCourse, saveCourse, deleteCourse } from "./controllers/course";
-import { listStudent, saveStudent, deleteStudent } from "./controllers/student";
-import { listTeacher, saveTeacher, deleteTeacher } from "./controllers/teacher";
-import { listSubject, saveSubject, deleteSubject } from "./controllers/subject";
-import { listProduct, saveProduct, deleteProduct } from "./controllers/product";
+
 import { listCar, saveCar, deleteCar } from "./controllers/car";
 import { listDriver, saveDriver, deleteDriver } from "./controllers/driver";
 const router = Router();
 
-router.get("/courses", listCourse);
-router.post("/courses", saveCourse);
-router.delete("/courses/:id", deleteCourse);
-
-router.get("/students", listStudent);
-router.post("/students", saveStudent);
-router.delete("/students/:id", deleteStudent);
-
-router.get("/teachers", listTeacher);
-router.post("/teachers", saveTeacher);
-router.delete("/teachers/:id", deleteTeacher)
-
-router.get("/subjects", listSubject);
-router.post("/subjects", saveSubject);
-router.delete("/subjects/:id", deleteSubject)
-
-router.get("/products", listProduct);
-router.post("/products", saveProduct);
-router.delete("/products/:id", deleteProduct)
 
 router.get("/cars", listCar);
 router.post("/cars", saveCar);
